9866536a655754c8fc2b384c997307995affc285
hqhung201 <hqhung201@gmail.com>
2022-03-27 09:36:40 +0700

    Send data to template file

--- myapp/views.py
+++ myapp/views.py
@@ -2,4 +2,9 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    return render(request, 'index.html')
+    context = {
+        'name': 'Hung',
+        'age': 20,
+        'nationality': 'Vietnamese'
+    }
+    return render(request, 'index.html', context)
